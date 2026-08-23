9f6ebc3bc6824dcb4a105cd10c418e6b2e4f42ad
iki1uc <entertrainer@web.de>
2026-08-23 12:23:08 +0200

    Create 6x6y.js

---
+++ 6x6y.js
@@ -0,0 +1,23 @@
+NC.engine = {
+  sense: {
+    aura: "MOTORIC.FIELD",
+    mana: "MOTORIC.IMPULSE",
+    meta: false
+  },
+  cube: {
+    mode: "6-SLOT",
+    faces: [1,2,3,4,5,6],
+    motoric: true,
+    relevance: "NC6"
+  },
+  layer: {
+    I: "SELF.NODE",
+    YOU: "EXTERNAL.NODE",
+    OUR: "SHARED.NODE",
+    EAR: "RECEIVE.NODE"
+  },
+  run: {
+    active: 3,
+    mode: "GLOBAL"
+  }
+}
